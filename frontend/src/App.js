a519b11008245a80a4f3b4be9041256dfacb2c5e
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-03-26 13:36:03 -0700

    Header background rough draft design for project

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header">
+        <div class="logo-box">
+          <img alt="Logo" class="logo" />
+        </div>
+        <div class="text-box">
+          <h1 class="heading-primary">
+            <span class="heading-primary-main">Produce Tracker</span>
+            <span class="heading-primary-sub">Secure Storage</span>
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
